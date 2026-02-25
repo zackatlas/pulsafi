c393ce87e9cef3fd871b27da60e324aaedb45666
zackatlas <zackeryatlas@berkeley.edu>
2026-02-24 22:50:29 -0800

    Update ArticleLayout.js

--- app/learn/ArticleLayout.js
+++ app/learn/ArticleLayout.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function ArticleLayout({ title, category, date, readTime, children }) {
   const [email, setEmail] = useState("");
