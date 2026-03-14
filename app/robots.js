a3eede67d01727f53c4e0a957be9adcb0dfdff32
Zackery Atlas <zack@Zackerys-Laptop.local>
2026-03-13 23:43:19 -0700

    Fix robots.txt to list all sitemap files directly

--- app/robots.js
+++ app/robots.js
@@ -7,6 +7,14 @@ export default function robots() {
         disallow: ["/widget", "/api/", "/dashboard"],
       },
     ],
-    sitemap: "https://pulsafi.com/sitemap.xml",
+    sitemap: [
+      "https://pulsafi.com/sitemap/0.xml",
+      "https://pulsafi.com/sitemap/1.xml",
+      "https://pulsafi.com/sitemap/2.xml",
+      "https://pulsafi.com/sitemap/3.xml",
+      "https://pulsafi.com/sitemap/4.xml",
+      "https://pulsafi.com/sitemap/5.xml",
+      "https://pulsafi.com/sitemap/6.xml",
+    ],
   };
 }
