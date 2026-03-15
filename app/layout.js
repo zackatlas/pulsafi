933e657930e3f2ba212ec3aa6df45e6d424e36f3
zackatlas <zackeryatlas@berkeley.edu>
2026-03-15 01:55:49 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { ThemeProvider } from './components/ThemeProvider'
 import AuthProvider from './components/AuthProvider'
 export const metadata = {
@@ -107,6 +108,7 @@ export default function RootLayout({ children }) {
           </AuthProvider>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
